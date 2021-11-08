9cea61e0b76d2ad8daa416b5677b2c68a06c14bb
Guilherme Kfouri <guiokfouri@gmail.com>
2021-11-08 18:41:07 -0300

    Create AddUserMVCActionCommand

---
+++ src/main/java/com/liferay/simplify/user/customfields/management/constants/MVCCommandNames.java
@@ -0,0 +1,6 @@
+package com.liferay.simplify.user.customfields.management.constants;
+
+public class MVCCommandNames {
+
+    public static final String ADD_USER = "/add_user";
+}
---
+++ src/main/java/com/liferay/simplify/user/customfields/management/portlet/action/AddUserMVCActionCommand.java
@@ -0,0 +1,61 @@
+package com.liferay.simplify.user.customfields.management.portlet.action;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.simplify.user.customfields.management.constants.MVCCommandNames;
+import com.liferay.simplify.user.customfields.management.constants.UserCustomFieldsManagementPortletKeys;
+import org.osgi.service.component.annotations.Component;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+@Component(
+        immediate = true,
+        property = {
+                "javax.portlet.name=" + UserCustomFieldsManagementPortletKeys.USERCUSTOMFIELDSMANAGEMENT,
+                "mvc.command.name=" + MVCCommandNames.ADD_USER
+        },
+        service = MVCActionCommand.class
+)
+public class AddUserMVCActionCommand extends BaseMVCActionCommand {
+
+    @Override
+    protected void doProcessAction(ActionRequest actionRequest, ActionResponse actionResponse) throws PortalException {
+        String firstName = ParamUtil.getString(actionRequest, "firstName");
+        String lastName = ParamUtil.getString(actionRequest, "lastName");
+        String screenName = ParamUtil.getString(actionRequest, "screenName");
+        String email = ParamUtil.getString(actionRequest, "email");
+        boolean gender = ParamUtil.getInteger(actionRequest, "gender") == 1;
+        int birthdayDay = ParamUtil.getInteger(actionRequest, "birthdayDay");
+        int birthdayMonth = ParamUtil.getInteger(actionRequest, "birthdayMonth");
+        int birthdayYear = ParamUtil.getInteger(actionRequest, "birthdayYear");
+
+        Map<String, Serializable> expandoBridgeAttributes = new HashMap<>();
+        String cpf = ParamUtil.getString(actionRequest, "cpf");
+        String booksPerYear = ParamUtil.getString(actionRequest, "booksPerYear");
+        expandoBridgeAttributes.put("cpf", cpf);
+        expandoBridgeAttributes.put("booksPerYear", booksPerYear);
+
+        ThemeDisplay themeDisplay = (ThemeDisplay) actionRequest.getAttribute(WebKeys.THEME_DISPLAY);
+        long userId = themeDisplay.getUserId();
+
+        ServiceContext serviceContext = ServiceContextFactory.getInstance(actionRequest);
+        serviceContext.setExpandoBridgeAttributes(expandoBridgeAttributes);
+
+        UserLocalServiceUtil.addUser(userId, themeDisplay.getCompanyId(), true, "",
+                "", false, screenName, email, Locale.getDefault(), firstName,
+                "", lastName, 0, 0, gender, birthdayMonth, birthdayDay, birthdayYear,
+                "", null, null, null, null, false, serviceContext);
+    }
+}
